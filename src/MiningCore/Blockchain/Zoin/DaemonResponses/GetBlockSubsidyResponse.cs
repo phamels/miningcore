7f9b6dc67edc1db7d224fc9ebade06557c9186a4
Pie <pie@netabuse.net>
2018-05-27 13:45:28 +0200

    Minor changes

---
+++ src/MiningCore/Blockchain/Zoin/DaemonResponses/GetBlockSubsidyResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiningCore.Blockchain.Zoin.DaemonResponses
+{
+    class ZoinBlockSubsidy
+    {
+        public decimal Amount { get; set; }
+        public string Payee { get; set; }
+        public string Script { get; set; }
+    }
+}
---
+++ src/MiningCore/Blockchain/Zoin/DaemonResponses/GetBlockTemplateResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiningCore.Blockchain.Zoin.DaemonResponses
+{
+    class GetBlockTemplateResponse : Bitcoin.DaemonResponses.BlockTemplate
+    {
+        public ZoinBlockSubsidy Subsidy { get; set; }
+    }
+}
